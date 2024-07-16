39e6d6f4fabfefb4c66e2169b476080e578fb210
Matheus Albuquerque <matheusalbuquerque@Matheuss-MacBook-Pro.local>
2024-07-16 17:30:13 -0300

    este commit adiciona uma chamada de um metodo nativo no kotlin para buscar as informacoes do dispositivos , e retorna em um mapa no flutter , salva essas informacoes no cache no GET storage

--- marvel/android/app/src/main/kotlin/com/example/marvel/MainActivity.kt
+++ marvel/android/app/src/main/kotlin/com/example/marvel/MainActivity.kt
@@ -1,5 +1,32 @@
 package com.example.marvel
 
+import android.os.Build
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "marvel.channel"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        // Set up the MethodChannel with the same name as defined in Dart
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getDeviceInfo") {
+                val deviceInfo = getDeviceInfo()
+                result.success(deviceInfo)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getDeviceInfo(): Map<String, String> {
+        return mapOf(
+                "model" to Build.MODEL,
+                "version" to Build.VERSION.RELEASE,
+                "sdkInt" to Build.VERSION.SDK_INT.toString()
+        )
+    }
+}
